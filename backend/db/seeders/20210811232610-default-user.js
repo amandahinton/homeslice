659f707dcd194ec0fc86e3c86e21cb52fbef3c10
Amanda Hinton <hello@amandahinton.com>
2021-08-15 14:08:04 -0700

    Update users db to match schema

--- backend/db/seeders/20210811232610-default-user.js
+++ backend/db/seeders/20210811232610-default-user.js
@@ -12,8 +12,6 @@ module.exports = {
         email: 'demo@user.io',
         phone: '123-456-7890',
         hashedPassword: bcrypt.hashSync('password'),
-        createdAt: new Date(),
-        updatedAt: new Date(),
       },
       {
         username: 'BowWow',
@@ -22,8 +20,6 @@ module.exports = {
         email: faker.internet.email(),
         phone: faker.internet.phone_number(),
         hashedPassword: bcrypt.hashSync('carrots'),
-        createdAt: new Date(),
-        updatedAt: new Date(),
       },
     ], {});
   },
