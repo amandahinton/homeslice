06e551b6eee6f928df4cd8348dd4d8a435dc753c
Amanda Hinton <hello@amandahinton.com>
2021-08-18 23:35:14 -0700

    Style all modal forms

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch } from 'react-redux';
+import "../../context/Modal.css"
 
 function LoginForm() {
   const dispatch = useDispatch();
@@ -19,30 +20,42 @@ function LoginForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        Username or Email
-        <input
-          type="text"
-          value={credential}      // controlled input for username/email
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}      // controlled input for password
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <div className="loginFormDiv">
+      <div className="formTitleDiv">
+        <h1 className="formTitle">Login</h1>
+      </div>
+      <div className="formFieldsDiv">
+        <form className="loginForm" onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+          </ul>
+          <label className="formLabel">
+            Username or Email
+            <input
+              className="formInput"
+              type="text"
+              value={credential}      // controlled input for username/email
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+          </label>
+          <label className="formLabel">
+            Password
+            <input
+              className="formInput"
+              type="password"
+              value={password}      // controlled input for password
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <button
+            className="formButton"
+            type="submit"
+          >Log In</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
