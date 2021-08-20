fb4fc72064883fd4ed1745b87d3d989694ef52e5
Amanda Hinton <hello@amandahinton.com>
2021-08-19 20:58:52 -0700

    Style homes list page

--- frontend/src/components/HomeDetail/index.js
+++ frontend/src/components/HomeDetail/index.js
@@ -6,15 +6,14 @@ const HomeDetail = ({ id }) => {
   const homeData = useSelector((state) => state.homes[id]);
   return (
     <li className="homeListItem">
-
-      <div className="homeDataDiv">
-        <NavLink to={`/homes/${id}`}><img className="homeDataPhoto" src={homeData?.photoUrl} alt="home" /></NavLink>
-        <NavLink to={`/homes/${id}`}><h2 className="homeDataAddress1">{homeData?.street}</h2></NavLink>
+      <NavLink className="homeDataDiv" to={`/homes/${id}`}>
+        <img className="homeDataPhoto" src={homeData?.photoUrl} alt="home" />
+        <h2 className="homeDataAddress1">{homeData?.street}</h2>
         <p className="homeDataAddress2">{homeData?.city}, {homeData?.state} {homeData?.zipcode}</p>
         <p className="homeDataSize">{homeData?.sqft} square feet</p>
         <p className="homeDataSize">{homeData?.beds} bedrooms, {homeData?.baths} bathrooms</p>
         <p className="homeDataYear">Built in {homeData?.yearBuilt}</p>
-      </div>
+      </NavLink>
     </li>
   );
 };
