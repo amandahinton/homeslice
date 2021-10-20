13176451fea760b1b7e63e93eef9a8ed92c9d7a1
Amanda Hinton <hello@amandahinton.com>
2021-10-20 13:14:19 -0700

    Make splash full-width background image cover

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,17 +1,21 @@
+// import splashback from './splash.jpg'
+
 const Splash = () => {
 
   return (
-    <div className="homepage">
+    <div className="homepage"
+      // style={{backgroundImage: `url(${splashback})`}}
+    >
       <div className="homepage-text">
         <h1 className="homepage-title">Welcome Home!</h1>
         <h2 className="homepage-description">
           Homeslice helps homeowners organize all of the maintenance and repair activities for their home.
         </h2>
         <h4 className="homepage-value">
-          Use our suggested tasks or create your own custom ones. Stay on top of the recurring needs of your property—from roof inspection to gutter cleaning to appliance upkeep—extending and protecting the value and safety of your home.
+          Schedule our suggested tasks or create your own. Stay on top of the recurring and one-time needs of your property—from roof inspection to gutter cleaning to appliance upkeep—extending and protecting the value and safety of your home.
         </h4>
       </div>
-      <img className="homepage-pic" src="/splash.png" alt="Welcome to Homeslice" />
+
     </div>
   );
 };
