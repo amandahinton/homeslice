c66144e6a0f979c3bd1c7d53f9fd087cd48a469d
Amanda <hello@amandahinton.com>
2026-07-09 18:04:57 -0400

    update year

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,7 +2,7 @@ const Footer = ( { isSplash }) => {
 
   return (
     <div className={isSplash ? "footerDiv" : "footerDiv screen-footer"}>
-      <p className="footerText">Copyright ©2021 All Rights Reserved.</p>
+      <p className="footerText">Copyright ©2026 All Rights Reserved.</p>
       <p className="footerLink"><a href={"https://amandahinton.com/"} target={"_blank"} rel={"noreferrer"}>Created by Amanda Hinton</a></p>
     </div>
   );
